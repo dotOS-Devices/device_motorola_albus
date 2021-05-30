2a62a0fef4c75be49bf7dedcaef72de0d6bd8a7f
Helton Vezzoni <Helton_Maximus@msn.com>
2021-05-30 18:18:08 -0400

    update fingerprit

--- dot_albus.mk
+++ dot_albus.mk
@@ -18,16 +18,27 @@ $(call inherit-product, device/motorola/albus/device.mk)
 
 # Inherit some common Pixel Experience stuff.
 $(call inherit-product, vendor/dot/config/common.mk)
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_BRAND := motorola
-PRODUCT_MANUFACTURER := motorola
 PRODUCT_NAME := dot_albus
+PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := albus
+PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := Moto Z2 Play
 
-# Build Fingerprint
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+# Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="albus" \
+    PRODUCT_NAME="albus" \
     PRIVATE_BUILD_DESC="albus-user 9 PPS29.133-30 ab8b4 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/crosshatch/crosshatch:11/RQ2A.210505.002/7246365:user/release-keys"
